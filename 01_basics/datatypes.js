e3d5f650b6cf189a0b0e1743c4a3a5691b54a472
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2026-01-09 00:24:14 +0530

    Add datatypes.js to understand JS data types
    Introduced a new JavaScript file to demonstrate various data types and their characteristics.

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+//Datatypes and ECMA standards
+
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") -> we're using nodejs, not browser
+
+
+console.log(3+3); console.log("ayushmaan") // code readability should be high
+
+// PRIMITIVE DATA TYPES:
+// range of number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value. null is an object data type
+// undefined => declared but no value. undefined is undefined data type in itself
+// symbol => unique
+
+
+// object
+
+let name = "ayushmaan" //string
+let age = 22 //number
+let isLoggedIn = true //boolean
