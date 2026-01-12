4f67be3ce471b871a22521a3bf38566a0ba2f057
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2026-01-12 22:46:47 +0530

    Add conversion and basic operation examples in JS
    This file demonstrates type conversion operations in JavaScript, including converting values to number, boolean, and string types, as well as basic arithmetic operations.

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+let score = null
+
+let valueInNumber = Number(score)
+
+console.log(typeof score)
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)
+
+// // "33" => 33
+// // "33abc" => NaN (Not a number), but type is number only.
+// // true => 1; false => 0
+// // null => 0
+
+let isLoggedIn = undefined
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// // 1 => true; 0 => flase
+// // "" => false; "ayush" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+
+// ****************************** Operations ****************************************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " ayush"
+
+let str3 = str1 + str2
+console.log(str3)
