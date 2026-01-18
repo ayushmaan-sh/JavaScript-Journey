7779437f8b52f450440242c4aff9d25fa44c09c8
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2026-01-18 15:12:21 +0530

    Array Practice Questions

--- 02_basics/05_arrayPractice.js
+++ 02_basics/05_arrayPractice.js
@@ -22,9 +22,40 @@ teaTypes[1] = "jasmine tea"
 console.log(teaTypes);
 
 
-//5. Declare an array named `citiesVisited` containing "Mumbai" and "Sydney".
-// Add Berlin to the array
+//5. You have an array named "teaOrders" with "chai", "iced tea", "matcha", and "earl grey"
+// Remove the last element of the array using the `pop` method and store it in a vriable named `lastOrder`.
 
-let citiesVisited = ["Mumbai", "Sydney"]
-citiesVisited.push("Berlin")
-console.log(citiesVisited);
+let teaOrders = ["chai", "iced tea", "matcha","earl grey"]
+
+let lastOrder = teaOrders[3]
+teaOrders.pop()
+let lastOrder = teaOrders.pop()
+console.log("Last Order: ",lastOrder);
+
+console.log(teaOrders);
+
+//6. You have an array named "pupularTeas", containing "green tea", "oolong tea" and "chai".
+// create a soft copy of this array and name it "softCopyTeas"
+
+let popularTeas = ["green tea", "oolong tea", "chai"]
+let softCopyTeas = popularTeas
+
+popularTeas.pop()//softCopyTea and popularTea is having the reference of same array in the memory. (softCopy tea is not refering to popularTea). So whatever change or activities we do, it's happen with both variables.
+
+console.log(softCopyTeas);
+console.log(popularTeas);
+
+//7. You have an array named topCities containing "Berlin", "Singapore" and "New York".
+// Create a hard copy of this array named "hardCopyCities"
+
+let topCities = ["Berling", "Singapore", "New York"]
+let hardCopyCities = [...topCities] //creates a separate copy of the array
+
+// let hardCopyCities = topCities.slice() -> another way
+
+//8. You have two arrays: "europeanCities" containing "Paris" and "Rome", and "asianCities" containing "tokyo" and "bangkok",
+//merge these arrays into a new array named "worldCities"
+let europeanCities = ["Paris", "Rome"]
+let asianCities = ["Tokyo", "Bangkok"]
+
+let worldCities = [...europeanCities, ...asianCities]
