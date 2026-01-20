d3565187653fba3df9fef03d0084a17a1e52f34c
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2026-01-20 23:17:39 +0530

    While and do-while loop
    Implement while and do-while loops for birthday and score messages.

---
+++ 04_controlFlow/03_whileLoop.js
@@ -0,0 +1,15 @@
+let number = 24
+while (number == 24) {
+    console.log("Number 24 detected: It's your birthday");
+    number++
+}
+
+//do while loop
+
+let score = 1
+do {
+    if (score == 10) {
+        console.log(`Max score reached : ${score}`);
+    }
+    score++
+} while (score <= 10);
