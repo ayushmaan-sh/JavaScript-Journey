de0417e28a5cd44ef777fbf7f09159c0f551362a
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2026-02-19 22:42:26 +0530

    Representation of https payload.
    Add comments explaining response payload and ports

---
+++ NodeJS/https.js
@@ -0,0 +1,23 @@
+/* 
+
+RESPONSE PAYLOAD-  Preview, Response
+
+Request URL: https://www.google.com/ -----------> An addres for the server
+Request Method: GET-----------------------------> Request Method
+Status Code: 200 OK-----------------------------> Status Code
+Remote Address: 163.70.140.35:443---------------> The real address of the server
+
+Response Headers--------------------------------> Headers sent from the request
+Request Headers---------------------------------> Headers Sent from the server
+
+
+PORTS: PORTS ARE LOGICAL ENDPOINTS USED BY PROTOCOLS TO IDENTITFY SPECIFIC PROCESSES RUNNING ON A COMPUTER OR SERVER.
+
+
+Client----->https://google.com:3000------>3000->Node process 1
+    |                                     3001->Node process 2
+    |                                     3002->Node process 3
+    |                                     80--->Node process 4
+    |------>https://google.com----------->443-->Node process 5
+
+*/
